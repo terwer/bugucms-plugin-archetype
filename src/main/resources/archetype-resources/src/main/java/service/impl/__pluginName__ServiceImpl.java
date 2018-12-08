de97403bc3b3b6932c34086cef11ffa9ca44bc3c
terwer <youweics@sina.com>
2018-12-09 00:41:18 +0800

    简化模板

--- src/main/resources/archetype-resources/src/main/java/service/impl/__pluginName__ServiceImpl.java
+++ src/main/resources/archetype-resources/src/main/java/service/impl/__pluginName__ServiceImpl.java
@@ -7,17 +7,13 @@
 package ${package}.service.impl;
 
 import com.alibaba.fastjson.JSON;
-import ${groupId}.core.CommonDAO;
+import com.terwergreen.core.CommonService;
 import ${package}.service.${pluginName}Service;
+import com.terwergreen.pojo.SiteConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * 业务实现
@@ -31,23 +27,14 @@ public class ${pluginName}ServiceImpl implements ${pluginName}Service {
     private static final Logger log = LoggerFactory.getLogger(${pluginName}ServiceImpl.class);
 
     @Autowired
-    private CommonDAO commonDAO;
+    private CommonService commonService;
 
     @Override
-    public String get${pluginName}Info() {
-        List list = null;
-        try {
-            Map paramMap = new HashMap();
-            paramMap.put("optionGroup", "siteConfig");
-            list = commonDAO.queryListByMap("getOptionByGroup", paramMap);
-        } catch (Exception e) {
-            log.error("获取配置项异常", e);
-            return "current version:BuguCMS 2.0.0";
-        }
-        //多个结果返回List，只有一个结果的时候直接返回
-        if (!CollectionUtils.isEmpty(list)) {
-            return JSON.toJSONString(list);
+    public String get{pluginName}Info() {
+        SiteConfig siteConfig = commonService.getSiteConfig();
+        if (null != siteConfig) {
+        return JSON.toJSONString(siteConfig);
         }
         return "no data";
+        }
     }
-}
